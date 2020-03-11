2013e0baee91f0f5af69ed8d01fbc7e805d996a6
FFFJason <42328055+FFFJason@users.noreply.github.com>
2020-03-11 14:31:00 +0800

    Add files via upload

--- code/Bert_CNN_Model.py
+++ code/Bert_CNN_Model.py
@@ -104,7 +104,7 @@ def f1_score(recall,precision):
     f1_score = (recall*precision*2)/(recall+precision)
     return f1_score
 
-data_bert = np.load('FMA1AllDataUniqueBert.npy',allow_pickle=True)
+data_a = np.load('FMA1AllDataUniqueBert.npy',allow_pickle=True)
 
 def load_file(path,A,B):
     dataFrame = pd.read_csv(path)
@@ -112,8 +112,8 @@ def load_file(path,A,B):
     text_B = dataFrame[B]
     return text_A,text_B
 
-child,parent = load_file('FMA1isa.csv','Child','Parent')
-n_child,n_parent = load_file('FMA1isaNegData.csv','Child','Parent')
+child,parent = load_file('FMA1AllData.csv','Child','Parent')
+n_child,n_parent = load_file('All_NDR_3Times1.csv','Child','Parent')
 test_all = list(np.load('FMA1AllDataUnique.npy',allow_pickle=True))
 
 pos_vects_a = []
@@ -124,12 +124,12 @@ for i in range(len(child)):
     zeros = []
     for j in range(num_a):
         zeros.append(np.zeros(768))
-    zeros.extend(data_bert[test_all.index(child[i])])
+    zeros.extend(data_a[test_all.index(child[i])])
     pos_vects_a.append(zeros)
     zeros = []
     for j in range(num_b):
         zeros.append(np.zeros(768))
-    zeros.extend(data_bert[test_all.index(parent[i])])
+    zeros.extend(data_a[test_all.index(parent[i])])
     pos_vects_b.append(zeros)
 
 data_a_all = np.zeros((len(pos_vects_a),20,768),dtype='float32')
@@ -149,12 +149,12 @@ for i in range(len(n_child)):
     zeros = []
     for j in range(num_a):
         zeros.append(np.zeros(768))
-    zeros.extend(data_bert[test_all.index(n_child[i])])
+    zeros.extend(data_a[test_all.index(n_child[i])])
     neg_vects_a.append(zeros)
     zeros = []
     for j in range(num_b):
         zeros.append(np.zeros(768))
-    zeros.extend(data_bert[test_all.index(n_parent[i])])
+    zeros.extend(data_a[test_all.index(n_parent[i])])
     neg_vects_b.append(zeros)
 data_a_neg = np.zeros((len(neg_vects_a),20,768),dtype='float32')
 for i in range(len(neg_vects_a)):
@@ -167,36 +167,6 @@ for i in range(len(neg_vects_b)):
 pos_vects = data_a_all - data_b_all
 neg_vects = data_a_neg - data_b_neg
 del data_a_all,data_b_all,data_a_neg,data_b_neg
-# pos_vects = []
-# for i in range(len(child)):
-#     num = 39 - len(child[i]) - len(parent[i])
-#     zeros = []
-#     for j in range(num):
-#         zeros.append(np.zeros(768,dtype='float32'))
-#     zeros.extend(data_bert[test_all.index(child[i])])
-#     zeros.append(np.zeros(768,dtype='float32'))
-#     zeros.extend(data_bert[test_all.index(parent[i])])
-#     pos_vects.append(zeros)
-# data_a = np.zeros((len(pos_vects),40,768),dtype='float32')
-# for i in range(len(pos_vects)):
-#     for j in range(len(pos_vects[i])):
-#         data_a[i][j] = pos_vects[i][j]
-
-# neg_vects = []
-# for i in range(len(n_child)):
-#     num = 39 - len(n_child[i]) - len(n_parent[i])
-#     zeros = []
-#     for j in range(num):
-#         zeros.append(np.zeros(768,dtype='float32'))
-#     zeros.extend(data_bert[test_all.index(n_child[i])])
-#     zeros.append(np.zeros(768,dtype='float32'))
-#     zeros.extend(data_bert[test_all.index(n_parent[i])])
-#     neg_vects.append(zeros)
-# data_b = np.zeros((len(neg_vects),40,768),dtype='float32')
-# for i in range(len(neg_vects)):
-#     for j in range(len(neg_vects[i])):
-#         data_b[i][j] = neg_vects[i][j]
-
 data = []
 data.extend(pos_vects)
 data.extend(neg_vects)
@@ -267,8 +237,8 @@ del pos_vects,neg_vects
 # rib_b_all = None
 
 print(data.shape)
-# labels = [1]*104488+[2]*61375+[0]*165863
-labels = [1]*104488+[0]*104488
+labels = [1]*104488+[2]*61375+[0]*497006
+# labels = [1]*104488+[2]*104488
 labels = to_categorical(np.asarray(labels))
 # rib_labels = [1]*346 + [2]*651 + [0]*812
 # rib_labels = to_categorical(np.asarray(rib_labels))
@@ -279,33 +249,44 @@ x_train,x_test,y_train,y_test = train_test_split(data,labels,test_size=0.1)
 # y_train = np.concatenate((y_train,rib_labels),axis = 0)
 
 model = Sequential()
-#model.add(embedding_layer)
-model.add(Bidirectional(LSTM(32,return_sequences=True),merge_mode='concat'))
-model.add(Dropout(0.5))
+model.add(Conv1D(256, 5, padding='same'))
+model.add(MaxPooling1D(3, 3, padding='same'))
+model.add(Conv1D(128, 5, padding='same'))
+model.add(MaxPooling1D(3, 3, padding='same'))
+model.add(Conv1D(64, 3, padding='same'))
+model.add(MaxPooling1D(3, 3, padding='same'))
+model.add(Conv1D(32, 3, padding='same'))
+model.add(MaxPooling1D(3, 3, padding='same'))
+model.add(Conv1D(16, 3, padding='same'))
+model.add(MaxPooling1D(3, 3, padding='same'))
+model.add(Conv1D(8, 3, padding='same'))
 model.add(Flatten())
-model.add(Dense(2,activation='sigmoid'))
-model.compile(loss='binary_crossentropy',optimizer='adam',metrics=['accuracy'])
-
+model.add(Dropout(0.1))
+model.add(BatchNormalization())  
+model.add(Dense(256, activation='relu'))
+model.add(Dropout(0.1))
+model.add(Dense(3, activation='softmax'))
+model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy'])
 history = model.fit(x_train,y_train,epochs=50,batch_size=512,validation_data=(x_test,y_test))
-model.save('FMA1BERTLSTMAllData_minus.h5')
+model.save('FMA1CNN_AllWithRandomNdr_Model.h5')
 y_pred = model.predict(x_test)
 y_pred = list(map(lambda x: x==max(x),y_pred)) * np.ones(shape=y_pred.shape)
 y_pred1 = []
 y_test1 = []
 for i in range(len(y_pred)):    
-    if y_pred[i].reshape(2)[0] == 1.:
+    if y_pred[i].reshape(3)[0] == 1.:
         y_pred1.append(0)
-    elif y_pred[i].reshape(2)[1] == 1.:
+    elif y_pred[i].reshape(3)[1] == 1.:
         y_pred1.append(1)
-    # elif y_pred[i].reshape(3)[2] == 1:
-    #     y_pred1.append(2)
+    elif y_pred[i].reshape(3)[2] == 1:
+        y_pred1.append(2)
 for i in range(len(y_test)):
-    if y_test[i].reshape(2)[0] == 1.:
+    if y_test[i].reshape(3)[0] == 1.:
         y_test1.append(0)
-    elif y_test[i].reshape(2)[1] == 1.:
+    elif y_test[i].reshape(3)[1] == 1.:
         y_test1.append(1)
-    # elif y_test[i].reshape(3)[2] == 1:
-    #     y_test1.append(2)
+    elif y_test[i].reshape(3)[2] == 1:
+        y_test1.append(2)
 
 print(sklearn.metrics.confusion_matrix(y_test1,y_pred1))
 print(sklearn.metrics.classification_report(y_test1, y_pred1))
@@ -315,13 +296,19 @@ tp = 0
 fn = 0
 
 for i in range(len(y_pred)):
-    if y_pred1[i]== 1 and y_test1[i]==1:
+    if (y_pred1[i]== 1 and y_test1[i]==1 ) or (y_pred1[i]==2 and y_test1[i]==2):
         tp+=1
-    elif y_pred1[i]== 1 and y_test1[i]==0 :
+    elif (y_pred1[i]== 1 or y_pred1[i]==2) and y_test1[i]==0 :
         fp+=1
-    elif y_pred1[i] == 0 and y_test1[i]==1:
+    elif y_pred1[i] == 0 and (y_test1[i]==1 or y_test1[i]==2):
         fn+=1
-
+# for i in range(len(y_pred)):
+#     if y_pred1[i]== 1 and y_test1[i]==1:
+#         tp+=1
+#     elif y_pred1[i]== 1 and y_test1[i]==0 :
+#         fp+=1
+#     elif y_pred1[i] == 0 and y_test1[i]==1:
+#         fn+=1
 recall_s = tp/(tp+fn)
 precision_s = tp/(tp+fp)
 f1 = (recall_s*precision_s*2)/(recall_s+precision_s)
